e4e9a0df8e54bbce5f746a9f618016a73a6a5951
kirian <kirianguiller@gmail.com>
2021-05-19 18:12:03 +0200

    add python3.7 specific installations lines

--- installations/python_dependencies.sh
+++ installations/python_dependencies.sh
@@ -1,9 +1,15 @@
+# install python 3.7
+sudo apt install python3.7-minimal
+
+# update core packages
+python3.7 -m pip install --upgrade pip wheel setuptools
+
 # install sacrebleu
-pip3 install sacrebleu --user
+python3.7 -m pip install sacrebleu --user
 
 
 # install mtdata
 cd ~ 
 git clone https://github.com/thammegowda/mtdata
 cd mtdata
-pip3 install --editable .
+python3.7 -m pip install --editable .
