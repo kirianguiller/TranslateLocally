55a6ce129670c81a3111b616b8bbc11e70616172
kirian <kirianguiller@gmail.com>
2021-05-19 18:20:45 +0200

    add comment in python install script

--- installations/python_dependencies.sh
+++ installations/python_dependencies.sh
@@ -1,3 +1,6 @@
+# Usually I create venv to be sure to not corrupt the global python ...
+# ... but let's just do it like this at the moment
+
 # install python 3.7
 sudo apt install python3.7-minimal
 
@@ -5,7 +8,7 @@ sudo apt install python3.7-minimal
 python3.7 -m pip install --upgrade pip wheel setuptools
 
 # install sacrebleu
-python3.7 -m pip install sacrebleu --user
+python3.7 -m pip install sacrebleu 
 
 
 # install mtdata
