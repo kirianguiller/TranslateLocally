003595bccd0f582060363a275821c354dedb815b
kirian <kirianguiller@gmail.com>
2021-06-09 14:28:43 +0000

    update installations scripts

--- installations/1_ubuntu_18-04_fresh_install.sh
+++ installations/1_ubuntu_18-04_fresh_install.sh
@@ -1,23 +1,24 @@
 set -e
 
 # install tons of stuff
+apt-get update
 apt-get install -y --no-install-recommends doxygen g++-8 ca-certificates git wget gnupg build-essential lsb-release g++-8 automake autogen libtool cmake-data cmake unzip libboost-all-dev libblas-dev libopenblas-dev libz-dev libssl-dev protobuf-compiler libprotobuf-dev
 
 # add for filtering scripts
-apt-get install parallel
-apt-get install pigz
+apt-get install -y --no-install-recommends parallel 
+apt-get install -y --no-install-recommends pigz 
 
 # update g++
 update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-8 8
 
 # update CMAKE (https://stackoverflow.com/questions/49859457/how-to-reinstall-the-latest-cmake-version)
 sudo apt-get update
-sudo apt-get install apt-transport-https ca-certificates gnupg \
+sudo apt-get install -y --no-install-recommends apt-transport-https ca-certificates gnupg \
                          software-properties-common wget
 wget -qO - https://apt.kitware.com/keys/kitware-archive-latest.asc |     sudo apt-key add -
 sudo apt-add-repository 'deb https://apt.kitware.com/ubuntu/ bionic main'
 sudo apt-get update
-sudo apt-get install cmake
+sudo apt-get install -y --no-install-recommends cmake
 
 # install cuda 11-1
 cd ~
